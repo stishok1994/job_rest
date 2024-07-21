3ef473605fcf30712880e59e426edbc820d7c15c
Anatoly <104995978+stishok1994@users.noreply.github.com>
2024-07-21 21:55:37 +0300

    'start_authorization'

--- js/authorization.js
+++ js/authorization.js
@@ -1,21 +1,21 @@
 // request authorization
-async function login (phone, password) {
+
+const AuthURL = 'https://pinzeria.tw1.ru/auth/login/'
+
+
+async function login (userData) {
     try {
-        const responce = await fetch ('http://147.45.109.158:8881/auth/login/', {
+        const responce = await fetch (AuthURL, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-                // phone, password 
-                username: phone,
-                password: password
-            })
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
             });
     
             if (responce.ok) {
                 const data = await responce.json();
-                return data.token // get token
+                return data.access_token // get token
             }
             else {
                 throw new Error('Ошибка авторизации');
@@ -31,25 +31,75 @@ function saveToken (token) {
     localStorage.setItem('authToken', token);
 }
 
-
 async function handleLogin () {
     try {
         const userName = document.getElementById ('id-phone-input').value;
         const password = document.getElementById('id-password-input').value;
 
-        console.log('Запрос:', userName, password);
+        const userData = {
+            username: userName,
+            password: password
+        };
+        console.log('Запрос:', userData);
 
-        const token = await login(userName,password)
+        const token = await login(userData)
         saveToken(token)
 
         console.log('Токен:', localStorage.getItem('authToken'));
     }
     catch (error) {
+        // alert('Error')
         console.error(error);
         alert('Ошибка авторизации: ' + error.message);
       }
 }
 
-// action input
 document.getElementById('btn-input_id').addEventListener('click', handleLogin);
 
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const loginForm = document.getElementById('login-form');
+//     const errorMessage = document.getElementById('error-message');
+
+//     loginForm.addEventListener('submit', async (event) => {
+//         event.preventDefault();
+
+//         const username = document.getElementById('username').value;
+//         const password = document.getElementById('password').value;
+
+//         const data = {
+//             username: username,
+//             password: password
+//         };
+
+//         try {
+//             const response = await fetch(AuthURL, {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify(data)
+//             });
+
+//             if (!response.ok) {
+//                 throw new Error('Ошибка авторизации');
+//             }
+
+//             const result = await response.json();
+//             console.log('Авторизация успешна', result);
+
+//             // Здесь можно перенаправить пользователя на другую страницу
+//             // window.location.href = 'dashboard.html';
+//         } catch (error) {
+//             errorMessage.textContent = error.message;
+//         }
+//     });
+// });
