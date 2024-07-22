a14bd4e9f8a2e1454e955ff9d76516e20bc1e4eb
Anatoly <104995978+stishok1994@users.noreply.github.com>
2024-07-23 00:43:34 +0300

    'authentication_start'

--- js/authorization.js
+++ js/authorization.js
@@ -3,6 +3,42 @@
 const AuthURL = 'https://pinzeria.tw1.ru/auth/login/'
 
 
+// Проверяем авторизацию
+document.addEventListener('DOMContentLoaded', ViewHideAccount());
+// Авторизация
+document.getElementById('btn-input_id').addEventListener('click', handleLogin);
+
+
+
+// Проверяем наличие авторизации (токена)
+
+function ViewHideAccount () {
+    console.log('test')
+    const accountSection1 = document.getElementById('account-success_1');
+    const accountSection2 = document.getElementById('account-success_2');
+    const loginSection1 = document.getElementById('logIn-action_1');
+    const loginSection2 = document.getElementById('logIn-action_2');
+
+    // проверяем наличие токена
+    const token = localStorage.getItem('authToken')
+
+    if (token) {
+        // Если токен существует, показываем блок личного кабинета и скрываем блок регистрации/входа
+        loginSection1.classList.add('view-hide');
+        loginSection2.classList.add('view-hide');
+        accountSection1.classList.remove('view-hide')
+        accountSection2.classList.remove('view-hide')
+    }
+    else {
+        // Иначе показываем блок авторизации
+        loginSection1.classList.remove('view-hide');
+        loginSection2.classList.remove('view-hide');
+        accountSection1.classList.add('view-hide')
+        accountSection2.classList.add('view-hide')
+    }  
+}
+
+
 async function login (userData) {
     try {
         const responce = await fetch (AuthURL, {
@@ -40,11 +76,11 @@ async function handleLogin () {
             username: userName,
             password: password
         };
-        console.log('Запрос:', userData);
+        // console.log('Запрос:', userData);
 
         const token = await login(userData)
         saveToken(token)
-
+        ViewHideAccount ();
         console.log('Токен:', localStorage.getItem('authToken'));
     }
     catch (error) {
@@ -54,7 +90,7 @@ async function handleLogin () {
       }
 }
 
-document.getElementById('btn-input_id').addEventListener('click', handleLogin);
+
 
 
 
