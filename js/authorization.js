9a000da7a757c6e6de3acb0d1e865822ab93f06e
Anatoly <104995978+stishok1994@users.noreply.github.com>
2024-07-30 21:37:48 +0300

    'login_finish2'

--- js/authorization.js
+++ js/authorization.js
@@ -6,10 +6,87 @@ const RegURL = 'https://pinzeria.tw1.ru/auth/register/'
 
 // Проверяем авторизацию
 document.addEventListener('DOMContentLoaded', ViewHideAccount());
-// Авторизация
-document.getElementById('btn-input_id').addEventListener('click', handleLogin);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Валидация и авторизация
+    const formLogin = document.getElementById('authorization-form');
+    const phoneLogIn = document.getElementById('id-phone-input_login');
+    const passwordLogIn = document.getElementById('id-password-input_login');
+    const phoneErrorMessageLogIn = document.getElementById('phone-error-message_login');
+    const passwordErrorMessageLogIn = document.getElementById('password-error-message_login');
+
+    formLogin.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        // валидация номера телефона
+        if (phoneLogIn.value.length !== 11) {
+            phoneLogIn.classList.add('is-invalid');
+            phoneErrorMessageLogIn.style.display = 'block';
+        } else {
+            phoneLogIn.classList.remove('is-invalid');
+            phoneErrorMessageLogIn.style.display = 'none';
+        }
+
+        // валидация пароля
+        if (passwordLogIn.value.length < 5) {
+            passwordLogIn.classList.add('is-invalid');
+            passwordErrorMessageLogIn.style.display = 'block';
+        } else {
+            passwordLogIn.classList.remove('is-invalid');
+            passwordErrorMessageLogIn.style.display = 'none';
+        }
+
+        // Если все поля валидны, можно отправить форму
+        if (!phoneLogIn.classList.contains('is-invalid') && !passwordLogIn.classList.contains('is-invalid')) {
+        console.log('test123')
+        // Авторизация
+        handleLogin()
+        // закрытие модольного окна
+        hideModal ()
+        // document.getElementById('btn-input_id').addEventListener('click', handleLogin);
+        }
+    });
+  });
+
+
+//   функция закрытия модульного окна
+function hideModal () {
+    const modal = document.getElementById('inp-reg');
+    modal.classList.remove('show');
+    document.body.classList.remove('modal-open');
+    const backdrop = document.querySelector('.modal-backdrop');
+    if (backdrop) {
+    backdrop.remove();
+    }
+}
+
 
 document.getElementById('btn-reg').addEventListener('click', handleReg);
+
+
+async function handleLogin () {
+    try {
+        const userName = document.getElementById ('id-phone-input_login').value;
+        const password = document.getElementById('id-password-input_login').value;
+
+        const userData = {
+            username: userName,
+            password: password
+        };
+        // console.log('Запрос:', userData);
+
+        const token = await login(userData)
+        saveToken(token)
+        ViewHideAccount ();
+        console.log('Токен:', localStorage.getItem('authToken'));
+    }
+    catch (error) {
+        // alert('Error')
+        console.error(error);
+        alert('Ошибка авторизации: ' + error.message);
+      }
+}
 
 
 // Проверяем наличие авторизации (токена)
@@ -56,7 +133,7 @@ async function login (userData) {
                 return data.access_token // get token
             }
             else {
-                throw new Error('Ошибка авторизации');
+                throw new Error('Проверьте правильность ввода логина и пароля');
             }}
     catch (error) {
         console.error(error);
@@ -69,28 +146,7 @@ function saveToken (token) {
     localStorage.setItem('authToken', token);
 }
 
-async function handleLogin () {
-    try {
-        const userName = document.getElementById ('id-phone-input').value;
-        const password = document.getElementById('id-password-input').value;
 
-        const userData = {
-            username: userName,
-            password: password
-        };
-        // console.log('Запрос:', userData);
-
-        const token = await login(userData)
-        saveToken(token)
-        ViewHideAccount ();
-        console.log('Токен:', localStorage.getItem('authToken'));
-    }
-    catch (error) {
-        // alert('Error')
-        console.error(error);
-        alert('Ошибка авторизации: ' + error.message);
-      }
-}
 
 // Регистрация
 async function handleReg () {
